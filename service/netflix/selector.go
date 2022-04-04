178b7dc221ebb06575f48934cea6385ce89f2285
g3un <g3un98@gmail.com>
2022-04-04 17:57:11 +0900

    Add Netflix constants

---
+++ service/netflix/selector.go
@@ -0,0 +1,14 @@
+// Netflix 선택자 선언
+
+package netflix
+
+const (
+    SEL_LOGIN_ID = `input[id="id_userLoginId"]`
+    SEL_LOGIN_PW = `input[id="id_password"]`
+    SEL_LOGIN_BTN = `button[class="btn login-button btn-submit btn-small"]`
+    SEL_LOGIN_ERR = `div[class="ui-message-container ui-message-error"]`
+)
+
+const (
+    SEL_INFO_MEMBERSHIP = `div[data-uia="plan-section"] > section`
+)
