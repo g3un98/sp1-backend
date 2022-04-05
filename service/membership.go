29a96a2c51e438402f14d30b7d9bc940d7b91419
g3un <g3un98@gmail.com>
2022-04-05 16:42:23 +0900

    Declare structures

---
+++ service/membership.go
@@ -0,0 +1,8 @@
+// 멤버십 구조체 선언
+
+package service
+
+type Membership struct {
+    Type int `json:"type"`
+    Cost int `json:"cost"`
+}
---
+++ service/payment.go
@@ -0,0 +1,9 @@
+// Netflix 결제 방식 구조체 정의
+
+package service
+
+type Payment struct {
+    Type string `json:"type"`
+    Detail string `json:"detail"`
+    Next string `json:"next"`
+}
